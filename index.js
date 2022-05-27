fc63b7821bb3835cc5d0e568d937934ec3007c9f
Kurniawan Sudirman <kurniawanplusayahibu@gmail.com>
2022-05-27 20:42:11 +0700

    Kurniawan Sudirman: completed

--- index.js
+++ index.js
@@ -23,20 +23,30 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
-
+  let hasilLooping = [];
+  dataYangAkanDilooping.forEach(element => {
+    let fullName = `${element.namaDepan} ${element.namaBelakang}`;
+    hasilLooping.push(fullName);
+  });
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
-
-  /*
+ /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
 
+  let jumlahPria = 0;
+  let jumlahWanita = 0;
+  dataYangAkanDilooping.forEach(element => {
+    if (element.jenisKelamin == "M") {
+      jumlahPria++;
+    } else {
+      jumlahWanita++;
+    }
+  });
+  
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -48,8 +58,14 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
-
+  let komentar = "";
+  if(jumlahPria > jumlahWanita){
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if(jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
   // ! JANGAN DIMODIFIKASI
   return {
     hasilLooping,
